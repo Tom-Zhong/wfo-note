495dedce407725d34868e0769ba32d95aa4a81f6
Tom Zhong <814374557@qq.com>
2025-12-06 13:56:18 +0800

    feat: opt month selection

--- src/pages/Popup.tsx
+++ src/pages/Popup.tsx
@@ -57,6 +57,9 @@ export default function() {
             animate
             mode="multiple"
             selected={selected}
+            defaultMonth={new Date()}
+            startMonth={new Date(new Date().getFullYear(), new Date().getMonth() - 3)}
+            endMonth={new Date(new Date().getFullYear(), new Date().getMonth() + 3)}
             modifiers={{
               workDays: workDays,
               restDays: restDays,
@@ -66,22 +69,22 @@ export default function() {
               restDays: "my-booked-class1",
             }}
             disabled={{
-              dayOfWeek: [0, 6]
+              dayOfWeek: [0, 6],
             }}
             onSelect={setSelected}
             onMonthChange={(month) => console.log(month)}
           />
-          <div className='flex-row' style={{  width: '100%', gap: '10px', marginTop: '5px' }}>
+          <div className='flex-row' style={{  width: '100%', gap: '10px', marginTop: '20px' }}>
             <div className='flex-row' style={{ alignItems: 'center', gap: '5px' }}>
-              <div style={{ width: '10px', height: '10px', backgroundColor: 'green'}}></div>
+              <div style={{ width: '20px', height: '20px', backgroundColor: 'green'}}></div>
               <span>公司办公</span>
             </div>
             <div className='flex-row' style={{ alignItems: 'center', gap: '5px' }}>
-              <div style={{ width: '10px', height: '10px', backgroundColor: 'grey'}}></div>
+              <div style={{ width: '20px', height: '20px', backgroundColor: 'grey'}}></div>
               <span>休假</span>
             </div>
           </div>
-          <h3 style={{fontSize: '18px'}}>
+          <h3 style={{fontSize: '24px'}}>
             您本月的WFO已达到
             { Math.floor(workDays.length / (realWorkDays - restDays.length) * 100)} %
             </h3>
@@ -133,6 +136,9 @@ export default function() {
             <button style={{width: '100%', marginBottom: '10px'}} onClick={() => resetCurrentMonth()}>清除当月计划</button>
             <button style={{width: '100%', marginBottom: '10px'}}>导入上月计划</button>
             <button style={{width: '100%'}}>导出所有计划到Excel</button>
+            <p style={{marginTop: '15px', marginBottom: '0px'}}>* 仅支持追溯过去三个月和计划未来三月的WFO记录</p>
+            <p style={{marginBottom: '0px'}}>* 请根据实际工作安排合理安排WFO时间</p>
+            <p style={{marginTop: '0px'}}>* 本插件的WFO提示仅供参考，实际以公司的WFO为准</p>
           </div>
         </div>
       </div>
